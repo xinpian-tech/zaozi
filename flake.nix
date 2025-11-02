c6ba15509f1e680f1314ec8256cdd3c433525a57
Clo91eaf <Clo91eaf@qq.com>
2025-11-02 16:04:22 +0800

    [nix] fix insufficient java stack memory during scala compilation

--- flake.nix
+++ flake.nix
@@ -76,21 +76,21 @@
         devShells.default = pkgs.mkShell {
           inputsFrom = [ pkgs.zaozi.zaozi-assembly ];
           nativeBuildInputs = with pkgs; [ nixd ];
-          env = with pkgs; {
-            CIRCT_INSTALL_PATH = circt-install;
-            MLIR_INSTALL_PATH = mlir-install;
-            JEXTRACT_INSTALL_PATH = jextract-21;
-            LIT_INSTALL_PATH = lit;
-            SCALA_CLI_INSTALL_PATH = scala-cli;
-            RISCV_OPCODES_INSTALL_PATH = riscv-opcodes;
-          };
-          # pass to jextract
-          # Jextract splits the header class into multiple classes, which are combined via extending
-          # Due to https://github.com/scala/bug/issues/9272 we cannot access static fields in superclass headers, we work around this by not splitting the header
-          # https://github.com/openjdk/jextract/blob/8730fcf05c229d035b0db52ee6bd82622e9d03e9/src/main/java/org/openjdk/jextract/impl/ToplevelBuilder.java#L54
-          shellHook = ''
-            export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS --enable-preview -Djextract.decls.per.header=65535"
-          '';
+          env = with pkgs;
+            {
+              CIRCT_INSTALL_PATH = circt-install;
+              MLIR_INSTALL_PATH = mlir-install;
+              JEXTRACT_INSTALL_PATH = jextract-21;
+              LIT_INSTALL_PATH = lit;
+              SCALA_CLI_INSTALL_PATH = scala-cli;
+              RISCV_OPCODES_INSTALL_PATH = riscv-opcodes;
+              # pass to jextract
+              # Jextract splits the header class into multiple classes, which are combined via extending
+              # Due to https://github.com/scala/bug/issues/9272 we cannot access static fields in superclass headers, we work around this by not splitting the header
+              # https://github.com/openjdk/jextract/blob/8730fcf05c229d035b0db52ee6bd82622e9d03e9/src/main/java/org/openjdk/jextract/impl/ToplevelBuilder.java#L54
+              # rvprobe need a large stack size to compile
+              JAVA_TOOL_OPTIONS = "--enable-preview -Djextract.decls.per.header=65535 -Xss1G";
+            };
         };
       }
     );
