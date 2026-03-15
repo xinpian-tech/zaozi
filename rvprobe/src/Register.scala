3d2e4ad0f4477c84c1a035d9d9707d4f5b6489f1
Clo91eaf <Clo91eaf@qq.com>
2026-03-15 21:44:48 +0800

    feat: Introduce Register enum and update UpdateAsmApi script
    - Added a new Register enum to represent RISC-V registers (x0 to x31).
    - Updated UpdateAsmApi.scala to use the Register enum for specific argument names.
    - Modified the parameter types in generated instructions to use Register where applicable.
    - Adjusted constraints in generated instructions to reference the ordinal of Register enum values.
    - Updated tests in AsmApiTest.scala to reflect changes in instruction syntax, using Register enum instead of raw integers.

---
+++ rvprobe/src/Register.scala
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 Jianhao Ye <Clo91eaf@qq.com>
+package me.jiuyang.rvprobe
+
+enum Register:
+  case x0, x1, x2, x3, x4, x5, x6, x7
+  case x8, x9, x10, x11, x12, x13, x14, x15
+  case x16, x17, x18, x19, x20, x21, x22, x23
+  case x24, x25, x26, x27, x28, x29, x30, x31
