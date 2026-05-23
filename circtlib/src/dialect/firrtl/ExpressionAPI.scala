8286f4281f38c763d1745470c95bf9d58de8fd9d
Clo91eaf <Clo91eaf@qq.com>
2026-05-23 16:08:10 +0800

    [circtlib][ltl] remove unused LTL intrinsic classes and APIs

--- circtlib/src/dialect/firrtl/ExpressionAPI.scala
+++ circtlib/src/dialect/firrtl/ExpressionAPI.scala
@@ -157,58 +157,6 @@ class LEQPrim(val _operation: Operation)
 trait LEQPrimApi extends HasOperation[LEQPrim]
 end LEQPrimApi
 
-class LTLAndIntrinsic(val _operation: Operation)
-trait LTLAndIntrinsicApi extends HasOperation[LTLAndIntrinsic]
-end LTLAndIntrinsicApi
-
-class LTLClockIntrinsic(val _operation: Operation)
-trait LTLClockIntrinsicApi extends HasOperation[LTLClockIntrinsic]
-end LTLClockIntrinsicApi
-
-class LTLConcatIntrinsic(val _operation: Operation)
-trait LTLConcatIntrinsicApi extends HasOperation[LTLConcatIntrinsic]
-end LTLConcatIntrinsicApi
-
-class LTLDelayIntrinsic(val _operation: Operation)
-trait LTLDelayIntrinsicApi extends HasOperation[LTLDelayIntrinsic]
-end LTLDelayIntrinsicApi
-
-class LTLEventuallyIntrinsic(val _operation: Operation)
-trait LTLEventuallyIntrinsicApi extends HasOperation[LTLEventuallyIntrinsic]
-end LTLEventuallyIntrinsicApi
-
-class LTLGoToRepeatIntrinsic(val _operation: Operation)
-trait LTLGoToRepeatIntrinsicApi extends HasOperation[LTLGoToRepeatIntrinsic]
-end LTLGoToRepeatIntrinsicApi
-
-class LTLImplicationIntrinsic(val _operation: Operation)
-trait LTLImplicationIntrinsicApi extends HasOperation[LTLImplicationIntrinsic]
-end LTLImplicationIntrinsicApi
-
-class LTLIntersectIntrinsic(val _operation: Operation)
-trait LTLIntersectIntrinsicApi extends HasOperation[LTLIntersectIntrinsic]
-end LTLIntersectIntrinsicApi
-
-class LTLNonConsecutiveRepeatIntrinsic(val _operation: Operation)
-trait LTLNonConsecutiveRepeatIntrinsicApi extends HasOperation[LTLNonConsecutiveRepeatIntrinsic]
-end LTLNonConsecutiveRepeatIntrinsicApi
-
-class LTLNotIntrinsic(val _operation: Operation)
-trait LTLNotIntrinsicApi extends HasOperation[LTLNotIntrinsic]
-end LTLNotIntrinsicApi
-
-class LTLOrIntrinsic(val _operation: Operation)
-trait LTLOrIntrinsicApi extends HasOperation[LTLOrIntrinsic]
-end LTLOrIntrinsicApi
-
-class LTLRepeatIntrinsic(val _operation: Operation)
-trait LTLRepeatIntrinsicApi extends HasOperation[LTLRepeatIntrinsic]
-end LTLRepeatIntrinsicApi
-
-class LTLUntilIntrinsic(val _operation: Operation)
-trait LTLUntilIntrinsicApi extends HasOperation[LTLUntilIntrinsic]
-end LTLUntilIntrinsicApi
-
 class LTPrim(val _operation: Operation)
 trait LTPrimApi extends HasOperation[LTPrim]
 end LTPrimApi
