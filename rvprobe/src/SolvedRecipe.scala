c8ee1ea779cdf635981d6911cd39c65b649bfe01
Clo91eaf <Clo91eaf@qq.com>
2026-09-06 22:26:09 +0800

    refactor: remove rvprobe from the utlib branch

--- rvprobe/src/SolvedRecipe.scala
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2026 Jianhao Ye <Clo91eaf@qq.com>
-package me.jiuyang.rvprobe
-
-/** Solved instruction choices and arguments, with the assembly statement layout needed to render them. */
-final case class SolvedRecipe(
-  opcodes:    Map[Int, Int],
-  args:       Map[String, BigInt],
-  statements: Seq[Statement])
