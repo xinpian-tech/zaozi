94e6c4d6813c434ff4553ab29230f2d67a507539
Jiuyang Liu <liu@jiuyang.me>
2026-09-17 16:21:28 +0000

    syntheke: fix AXI register access error responses

--- syntheke/demo/src/zaoziimpl/AxiRegMap.scala
+++ syntheke/demo/src/zaoziimpl/AxiRegMap.scala
@@ -36,15 +36,18 @@ object AxiRegMap:
     val responseRead = RegInit(false.B)
     val responseInvalid = RegInit(false.B)
     val responseId = RegInit(0.B(shape.idBits))
+    val readRemaining = RegInit(0.U(8))
 
     val awAddr = Reg(Bits(shape.addrBits))
     val awId = Reg(Bits(shape.idBits))
+    val awSize = Reg(Bits(3))
     val awInvalid = Reg(Bool())
     val wData = Reg(Bits(32))
     val wMask = Reg(Bits(4))
     val wInvalid = RegInit(false.B)
     val arAddr = Reg(Bits(shape.addrBits))
     val arId = Reg(Bits(shape.idBits))
+    val arLen = Reg(Bits(8))
     val arSize = Reg(Bits(3))
     val arInvalid = Reg(Bool())
 
@@ -56,6 +59,20 @@ object AxiRegMap:
         ((size === 2.B(3)) & (addr.bits(1, 0) === 0.B(2)))
       (len === 0.B(8)) & aligned & ((burst === 0.B(2)) | (burst === 1.B(2)))
 
+    def accessMask(addr: Referable[Bits], size: Referable[Bits])(using Arena, Context, Block): Wire[Bits] =
+      val mask = Wire(Bits(4))
+      mask := 0.B(4)
+      for lane <- 0 until 4 do
+        when((size === 0.B(3)) & (addr.bits(1, 0) === lane.B(2))) {
+          mask := (1 << lane).B(4)
+        }
+      for lane <- Seq(0, 2) do
+        when((size === 1.B(3)) & (addr.bits(1, 0) === lane.B(2))) {
+          mask := (3 << lane).B(4)
+        }
+      when(size === 2.B(3)) { mask := 15.B(4) }
+      mask
+
     val collecting = !responding
     in.aw.ready := collecting & !pendingAw & !pendingAr
     in.w.ready := collecting & !pendingW & !pendingAr
@@ -65,6 +82,7 @@ object AxiRegMap:
       pendingAw := true.B
       awAddr := in.aw.bits.addr
       awId := in.aw.bits.id
+      awSize := in.aw.bits.size
       awInvalid := !validAddress(in.aw.bits.addr, in.aw.bits.len, in.aw.bits.size, in.aw.bits.burst)
     }
     when(in.w.valid & in.w.ready) {
@@ -80,6 +98,7 @@ object AxiRegMap:
       pendingAr := true.B
       arAddr := in.ar.bits.addr
       arId := in.ar.bits.id
+      arLen := in.ar.bits.len
       arSize := in.ar.bits.size
       arInvalid := !validAddress(in.ar.bits.addr, in.ar.bits.len, in.ar.bits.size, in.ar.bits.burst)
     }
@@ -87,20 +106,12 @@ object AxiRegMap:
     val issueRead = pendingAr
     val issueWrite = pendingAw & pendingW
     val issue = collecting & (issueRead | issueWrite)
-    val invalid = issueRead ? (arInvalid, awInvalid | wInvalid)
+    val writeMask = accessMask(awAddr, awSize)
+    val invalidStrb = (wMask & ~writeMask) =/= 0.B(4)
+    val invalid = issueRead ? (arInvalid, awInvalid | wInvalid | invalidStrb)
     val address = issueRead ? (arAddr, awAddr)
 
-    val readMask = Wire(Bits(4))
-    readMask := 0.B(4)
-    for lane <- 0 until 4 do
-      when((arSize === 0.B(3)) & (arAddr.bits(1, 0) === lane.B(2))) {
-        readMask := (1 << lane).B(4)
-      }
-    for lane <- Seq(0, 2) do
-      when((arSize === 1.B(3)) & (arAddr.bits(1, 0) === lane.B(2))) {
-        readMask := (3 << lane).B(4)
-      }
-    when(arSize === 2.B(3)) { readMask := 15.B(4) }
+    val readMask = accessMask(arAddr, arSize)
 
     req.valid := issue & !invalid
     req.bits.read := issueRead
@@ -117,6 +128,7 @@ object AxiRegMap:
       responseRead := issueRead
       responseInvalid := invalid
       responseId := issueRead ? (arId, awId)
+      when(issueRead) { readRemaining := arLen.asUInt }
     }
 
     val responseValid = responding & (responseInvalid | rsp.valid)
@@ -128,10 +140,14 @@ object AxiRegMap:
     in.r.bits.id := responseId
     in.r.bits.resp := responseError ? (2.B(2), 0.B(2))
     in.r.bits.data := responseInvalid ? (0.B(32), rsp.bits.data)
-    in.r.bits.last := true.B
+    in.r.bits.last := !responseInvalid | (readRemaining === 0.U(8))
     rsp.ready := responding & !responseInvalid & (responseRead ? (in.r.ready, in.b.ready))
-    when((in.r.valid & in.r.ready) | (in.b.valid & in.b.ready)) {
-      responding := false.B
+    when(in.r.valid & in.r.ready & responseInvalid & (readRemaining =/= 0.U(8))) {
+      readRemaining := (readRemaining - 1.U(8)).asBits.bits(7, 0).asUInt
+    }.otherwise {
+      when((in.r.valid & in.r.ready) | (in.b.valid & in.b.ready)) {
+        responding := false.B
+      }
     }
 
     (req, rsp)
